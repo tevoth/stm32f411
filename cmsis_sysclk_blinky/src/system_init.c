96310b4a8be62fb83b92403d6a0d7d68c05ad961
Tom Voth <tvoth3@gmail.com>
2026-02-04 11:19:15 -0700

    remove debugging variables

--- cmsis_sysclk_blinky/src/system_init.c
+++ cmsis_sysclk_blinky/src/system_init.c
@@ -3,15 +3,12 @@
 
 void system_init(void)
 {
-    volatile int64_t rcc_cr = RCC->CR;
     RCC->CR |= RCC_CR_HSION;
     while (!(RCC->CR & RCC_CR_HSIRDY));
 
-    volatile int64_t rcc_cfgr = RCC->CFGR;
     RCC->CFGR &= ~RCC_CFGR_SW;
     RCC->CFGR |= RCC_CFGR_SW_HSI;
 
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
-
 }
 
