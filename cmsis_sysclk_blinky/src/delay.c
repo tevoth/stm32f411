1de4b5f57df075bb1649581c5ad1152367e93e6d
Tom Voth <tvoth3@gmail.com>
2026-02-04 11:17:48 -0700

    cleanup

--- cmsis_sysclk_blinky/src/delay.c
+++ cmsis_sysclk_blinky/src/delay.c
@@ -4,10 +4,11 @@
 #define CTRL_COUNTFLAG (1U<<16)
 
 void delay(uint32_t delay) {
-  SysTick->CTRL = 0;
+  SysTick->CTRL = 0; // clear clock
   SysTick->VAL  = 0;
   SysTick->LOAD = 15999;
-  SysTick->CTRL = 5;
+  SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk; // use internal clock
+  SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;    // enable clock
 
   for (uint32_t i = 0; i < delay; i++) {
     while((SysTick->CTRL & CTRL_COUNTFLAG) == 0) {}
