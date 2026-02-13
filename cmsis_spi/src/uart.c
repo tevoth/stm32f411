f7a465ecdd10a35a2050ebaf504b46a24dabfdda
Tom Voth <tvoth3@yahoo.com>
2026-02-13 13:38:55 -0700

    move uart pin to PA2. PA9 conflicted with SPI pins

--- cmsis_spi/src/uart.c
+++ cmsis_spi/src/uart.c
@@ -24,27 +24,27 @@ int _write(int file, char *ptr, int len) {
 void uart_init(void) {
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-  GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR9_1);
+  // GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR2_0 | GPIO_PUPDR_PUPDR2_1);
   // set to alternative function
-  GPIOA->MODER &= ~GPIO_MODER_MODER9_0;
-  GPIOA->MODER |=  GPIO_MODER_MODER9_1;
-  // set PA9 to AF7
-  GPIOA->AFR[1] &= ~(0xF << GPIO_AFRH_AFSEL9_Pos); // Clear all 4 bits for PA9
-  GPIOA->AFR[1] |=  (0x7 << GPIO_AFRH_AFSEL9_Pos); // Set PA9 to AF7
+  GPIOA->MODER &= ~GPIO_MODER_MODER2_0;
+  GPIOA->MODER |=  GPIO_MODER_MODER2_1;
+  // set PA2 to AF7
+  GPIOA->AFR[0] &= ~(0xF << GPIO_AFRL_AFSEL2_Pos); // Clear all 4 bits for PA2
+  GPIOA->AFR[0] |=  (0x7 << GPIO_AFRL_AFSEL2_Pos); // Set PA2 to AF7
 
   // enable clock to access uart2
-  RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+  RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
   uart_set_baudrate(APB1_CLK, UART_BAUDRATE);
 
-  USART1->CR1 |= USART_CR1_TE;
-  USART1->CR1 |= USART_CR1_UE;
+  USART2->CR1 |= USART_CR1_TE;
+  USART2->CR1 |= USART_CR1_UE;
 }
 
 static void uart_write(int ch) {
-  while (!(USART1->SR & USART_SR_TXE)){}
+  while (!(USART2->SR & USART_SR_TXE)){}
 
-  USART1->DR = (ch & 0xFF);
+  USART2->DR = (ch & 0xFF);
 }
 
 static uint16_t compute_uart_bd(uint32_t periph_clk, uint32_t baudrate) {
@@ -53,5 +53,5 @@ static uint16_t compute_uart_bd(uint32_t periph_clk, uint32_t baudrate) {
 }
 
 static void uart_set_baudrate(uint32_t periph_clk, uint32_t baudrate) {
-  USART1->BRR = compute_uart_bd(periph_clk, baudrate);
+  USART2->BRR = compute_uart_bd(periph_clk, baudrate);
 }
