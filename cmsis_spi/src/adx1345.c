0284506d74d029a96c16534c00cc109bebd396c6
Tom Voth <tvoth3@gmail.com>
2026-02-13 11:43:42 -0700

    adding stm32 spi code

---
+++ cmsis_spi/src/adx1345.c
@@ -0,0 +1,61 @@
+#include "adx1345.h"
+
+void adxl_read(uint8_t address, uint8_t * rxdata)
+{
+  /*Set read operation*/
+  address |= ADXL345_READ_OPERATION;
+
+  /*Enable multi-byte*/
+  address |= ADXL345_MULTI_BYTE_ENABLE;
+
+  /*Pull cs line low to enable slave*/
+  cs_enable();
+
+  /*Send address*/
+  spi1_transmit(&address,1);
+
+  /*Read 6 bytes */
+  spi1_receive(rxdata,6);
+
+  /*Pull cs line high to disable slave*/
+  cs_disable();
+}
+
+void adxl_write (uint8_t address, uint8_t value)
+{
+  uint8_t data[2];
+  /*Enable multi-byte, place address into buffer*/
+  data[0] = address|ADXL345_MULTI_BYTE_ENABLE;
+
+  /*Place data into buffer*/
+  data[1] = value;
+
+  /*Pull cs line low to enable slave*/
+  cs_enable();
+
+  /*Transmit data and address*/
+  spi1_transmit(data, 2);
+
+  /*Pull cs line high to disable slave*/
+  cs_disable();
+}
+
+void adxl_init (void)
+{
+  /*Enable SPI gpio*/
+  spi_gpio_init();
+
+  /*Config SPI*/
+  spi1_config();
+
+  /*Set data format range to +-4g*/
+  adxl_write (ADXL345_REG_DATA_FORMAT, ADXL345_RANGE_4G);
+
+  /*Reset all bits*/
+  adxl_write (ADXL345_REG_POWER_CTL, ADXL345_RESET);
+
+  /*Configure power control measure bit*/
+  adxl_write (ADXL345_REG_POWER_CTL, ADXL345_MEASURE_BIT);
+}
+
+
---
+++ cmsis_spi/src/uart.c
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+
+#define UART_BAUDRATE 115200
+#define SYS_FREQ      16000000
+#define APB1_CLK      SYS_FREQ
+
+static void uart_set_baudrate(uint32_t periph_clk, uint32_t baudrate);
+static void uart_write(int ch);
+
+int __io_putchar(int ch) {
+  uart_write(ch);
+  return ch;
+}
+
+int _write(int file, char *ptr, int len) {
+    for (int i = 0; i < len; i++) {
+        __io_putchar(*ptr++);
+    }
+    return len;
+}
+
+void uart_init(void) {
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+  GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR9_0 | GPIO_PUPDR_PUPDR9_1);
+  // set to alternative function
+  GPIOA->MODER &= ~GPIO_MODER_MODER9_0;
+  GPIOA->MODER |=  GPIO_MODER_MODER9_1;
+  // set PA9 to AF7
+  GPIOA->AFR[1] &= ~(0xF << GPIO_AFRH_AFSEL9_Pos); // Clear all 4 bits for PA9
+  GPIOA->AFR[1] |=  (0x7 << GPIO_AFRH_AFSEL9_Pos); // Set PA9 to AF7
+
+  // enable clock to access uart2
+  RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
+  uart_set_baudrate(APB1_CLK, UART_BAUDRATE);
+
+  USART1->CR1 |= USART_CR1_TE;
+  USART1->CR1 |= USART_CR1_UE;
+}
+
+static void uart_write(int ch) {
+  while (!(USART1->SR & USART_SR_TXE)){}
+
+  USART1->DR = (ch & 0xFF);
+}
+
+static uint16_t compute_uart_bd(uint32_t periph_clk, uint32_t baudrate) {
+  uint16_t bd = ((periph_clk + (baudrate/2U))/baudrate);  
+  return bd;
+}
+
+static void uart_set_baudrate(uint32_t periph_clk, uint32_t baudrate) {
+  USART1->BRR = compute_uart_bd(periph_clk, baudrate);
+}
