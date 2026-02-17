fd38b008e24dae608da4991dd288d513dbcf188a
tevoth <tvoth3@yahoo.com>
2026-02-17 12:00:24 -0700

    cmsis_spi: use SysTick 1ms delay in main loop

---
+++ cmsis_spi/src/systick_msec_delay.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+#define CTRL_COUNTFLAG (1U << 16)
+
+void systick_msec_delay(uint32_t delay_ms) {
+  const uint32_t reload = (SystemCoreClock / 1000U) - 1U;
+
+  SysTick->CTRL = 0;
+  SysTick->VAL = 0;
+  SysTick->LOAD = reload;
+  SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
+  SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+
+  for (uint32_t i = 0; i < delay_ms; i++) {
+    while ((SysTick->CTRL & CTRL_COUNTFLAG) == 0) {}
+  }
+
+  SysTick->CTRL = 0;
+}
