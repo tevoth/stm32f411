35de03b655a49f17b1e1c7727da5d0044c6b3dcc
Tom Voth <tvoth3@gmail.com>
2026-02-18 12:53:06 -0700

    Clear TIM2 UIF after forced update event

--- cmsis_tim2clk_blinky/src/tim2_1hz_init.c
+++ cmsis_tim2clk_blinky/src/tim2_1hz_init.c
@@ -8,6 +8,7 @@ void tim2_1hz_init(void) {
   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // enable timer 2
   TIM2->PSC = PRESCL - 1;
   TIM2->EGR |= TIM_EGR_UG; // update pre-scaler NOW
+  TIM2->SR &= ~TIM_SR_UIF; // clear UIF raised by UG to avoid a false first tick
   TIM2->ARR = PERIOD - 1;
   TIM2->CR1 = TIM_CR1_CEN; // start timer
   // timer debugging DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_TIM2_STOP;
