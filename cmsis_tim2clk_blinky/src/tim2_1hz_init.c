ae0557c00dc9547e22c78c27e6a02a541a62aec7
Tom Voth <tvoth3@gmail.com>
2026-02-18 16:23:44 -0700

    Reset TIM2 before init to stabilize first tick after warm reset

--- cmsis_tim2clk_blinky/src/tim2_1hz_init.c
+++ cmsis_tim2clk_blinky/src/tim2_1hz_init.c
@@ -6,7 +6,13 @@
 
 void tim2_1hz_init(void) {
   RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // enable timer 2
+
+  // Force TIM2 back to reset state so warm/debug resets start from known config.
+  RCC->APB1RSTR |= RCC_APB1RSTR_TIM2RST;
+  RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+
   TIM2->PSC = PRESCL - 1;
+  TIM2->CNT = 0;
   TIM2->EGR |= TIM_EGR_UG; // update pre-scaler NOW
   TIM2->SR &= ~TIM_SR_UIF; // clear UIF raised by UG to avoid a false first tick
   TIM2->ARR = PERIOD - 1;
