3a25e86136a787d7d30edaa91d3827fd433edd5e
Tom Voth <tvoth3@gmail.com>
2026-02-18 12:59:15 -0700

    Reset RCC bus prescalers in system_init

--- cmsis_sysclk_blinky/src/system_init.c
+++ cmsis_sysclk_blinky/src/system_init.c
@@ -6,9 +6,11 @@ void system_init(void)
     RCC->CR |= RCC_CR_HSION;
     while (!(RCC->CR & RCC_CR_HSIRDY));
 
+    // Normalize bus prescalers so timing is deterministic after soft/debug resets.
+    RCC->CFGR &= ~(RCC_CFGR_HPRE | RCC_CFGR_PPRE1 | RCC_CFGR_PPRE2);
+
     RCC->CFGR &= ~RCC_CFGR_SW;
     RCC->CFGR |= RCC_CFGR_SW_HSI;
 
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
 }
-
