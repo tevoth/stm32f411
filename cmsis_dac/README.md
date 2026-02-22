6d49b21a9dc9f85a7bb34b547d6e3b074f0f35d4
Tom Voth <tvoth3@yahoo.com>
2026-02-22 14:42:17 -0700

    refactor: rename cmsis_dac API/files to adc

--- cmsis_dac/README.md
+++ cmsis_dac/README.md
@@ -1,1 +1,1 @@
-A CMSIS blinky!
+A CMSIS ADC example (STM32F411 ADC1 on PA1) with UART output.
--- cmsis_dac/src/main.c
+++ cmsis_dac/src/main.c
@@ -3,19 +3,19 @@
 #include "stm32f4xx.h"
 #include "led.h"
 #include "uart.h"
-#include "dac.h"
+#include "adc.h"
 #include "system_init.h"
 
 int main(void) {
   system_init();
   led_init();
   uart_init();
-  dac_init();
-  dac_start();
+  adc_init();
+  adc_start();
   int count = 0;
   while(1) {
     led_toggle();
-    volatile unsigned int value = dac_read(); 
+    volatile unsigned int value = adc_read(); 
     printf("HELLO FROM STM32...%u\n",value); 
     count = count + 1;
     for (volatile int i = 0; i < 1000000; i++) {}
