cb0a5d16cb343167149e8a39945b7cae792dc306
Tom Voth <tvoth3@yahoo.com>
2026-02-24 16:05:26 -0700

    Normalize PC13 LED init to no-pull across projects

--- cmsis_blinky/src/main.c
+++ cmsis_blinky/src/main.c
@@ -15,6 +15,7 @@ int main(void) {
   // Enable clock access to GPIOC
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 
+  GPIOC->PUPDR  &= ~(GPIO_PUPDR_PUPD13_Msk);
   GPIOC->MODER  &= ~(GPIO_MODER_MODER13_Msk);
   GPIOC->MODER  |=  (GPIO_MODER_MODER13_0);
   GPIOC->OTYPER &= ~(GPIO_OTYPER_OT13);
