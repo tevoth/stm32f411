9fca67757625b21a6fe5bc1810af74566ae6b63a
Tom Voth <tvoth3@gmail.com>
2026-02-25 14:15:45 -0700

    Use non-blocking TIM2 UIF polling in main loop

--- cmsis_tim2clk_blinky/src/main.c
+++ cmsis_tim2clk_blinky/src/main.c
@@ -1,18 +1,8 @@
 #include <stdint.h>
-#include <stdbool.h>
 #include "stm32f4xx.h"
 #include "gpio.h"
 #include "system_init.h"
 #include "tim2_1hz_init.h"
-
-#define TIM_WAIT_LIMIT 100000U
-
-// Wait until status bits are set; false indicates timeout.
-static bool tim_wait_set(volatile uint32_t *reg, uint32_t mask) {
-  uint32_t timeout = TIM_WAIT_LIMIT;
-  while (((*reg & mask) == 0U) && (timeout-- > 0U)) {}
-  return ((*reg & mask) != 0U);
-}
 
 int main(void) {
   system_init();
@@ -21,11 +11,10 @@ int main(void) {
   while(1) {
     // TODO: UIF polling can miss an update if another overflow occurs
     // between observing UIF and clearing it; migrate to CNT rollover or IRQ.
-    if (!tim_wait_set(&TIM2->SR, TIM_SR_UIF)) {
-      continue;
+    if ((TIM2->SR & TIM_SR_UIF) != 0U) {
+      TIM2->SR = ~TIM_SR_UIF; // reset UIF flag without RMW on SR
+      led_toggle();
     }
-    TIM2->SR = ~TIM_SR_UIF; // reset UIF flag without RMW on SR
-    led_toggle();
   }
   return 0;
 }
